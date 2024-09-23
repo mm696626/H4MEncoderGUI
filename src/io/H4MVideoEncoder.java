cce39698c8785e29336a619510e9d95f2f6a882d
Matt McCullough <mmccullough6899@gmail.com>
2024-09-23 18:14:36 -0400

    Added deletion of bmp files after conversion

--- src/io/H4MVideoEncoder.java
+++ src/io/H4MVideoEncoder.java
@@ -7,7 +7,7 @@ public class H4MVideoEncoder {
     public void encodeVideo(String commandString, String videoFilePath) throws IOException {
 
         String dumpBMPCommand = "ffmpeg.exe -i" + " \"" + videoFilePath + "\"" + " frame%05d.bmp";
-        String[] command = {"cmd.exe", "/c", "start", "cmd.exe", "/c", "cd tools && " + dumpBMPCommand + " && " + commandString};
+        String[] command = {"cmd.exe", "/c", "start", "cmd.exe", "/c", "cd tools && " + dumpBMPCommand + " && " + commandString + " && *.bmp"};
         ProcessBuilder processBuilder = new ProcessBuilder(command);
         processBuilder.start();
     }
